c8b6797618dcded66ecacae9e5a42c04cdee7222
unknown <seaspatton13@gmail.com>
2019-11-12 20:55:28 -0500

    final logic/node.js

--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -50,18 +50,15 @@ function promptUserPurchase() {
             filter: Number
         }
     ]).then(function (input) {
-        // console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
 
         var item = input.item_id;
         var quantity = input.quantity;
 
-        // Query db to confirm that the given item ID exists in the desired quantity
         var queryStr = 'SELECT * FROM products WHERE ?';
 
         connection.query(queryStr, { item_id: item }, function (err, data) {
             if (err) throw err;
 
-            // If the user has selected an invalid item ID, data attay will be empty
             // console.log('data = ' + JSON.stringify(data));
 
             if (data.length === 0) {
@@ -72,15 +69,12 @@ function promptUserPurchase() {
                 var productData = data[0];
 
                 // console.log('productData = ' + JSON.stringify(productData));
-                // console.log('productData.stock_quantity = ' + productData.stock_quantity);
 
                 // If the quantity requested by the user is in stock
                 if (quantity <= productData.stock_quantity) {
                     console.log('Congratulations, the product you requested is in stock! Placing order!');
 
-                    // Construct the updating query string
                     var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
-                    // console.log('updateQueryStr = ' + updateQueryStr);
 
                     // Update the inventory
                     connection.query(updateQueryStr, function (err, data) {
@@ -105,9 +99,7 @@ function promptUserPurchase() {
     })
 }
 
-// displayInventory will retrieve the current inventory from the database and output it to the console
 function displayInventory() {
-    // console.log('___ENTER displayInventory___');
 
     // Construct the db query string
     queryStr = 'SELECT * FROM products';
@@ -139,7 +131,6 @@ function displayInventory() {
 
 // runBamazon will execute the main application logic
 function runBamazon() {
-    // console.log('___ENTER runBamazon___');
 
     // Display the available inventory
     displayInventory();
